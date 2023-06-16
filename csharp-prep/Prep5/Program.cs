de019ea255c1e466fc085ff10002890361437eaf
Jerry Hodges <jhodge10@byui.edu>
2023-06-15 19:01:48 -0600

    learning02 w02

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,45 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        DisplayWelcomeMessage();
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
 
-        string userName = PromptUserName();
-        int userNumber = PromptUserNumber();
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
 
-        int squaredNumber = SquareNumber(userNumber);
+        Resume myResume = new Resume();
+        myResume._name = "Allison Rose";
 
-        DisplayResult(userName, squaredNumber);
-    }
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
 
-    static void DisplayWelcomeMessage()
-    {
-        Console.WriteLine("Welcome to the program!");
-    }
-
-    static string PromptUserName()
-    {
-        Console.Write("Please enter your name: ");
-        string name = Console.ReadLine();
-
-        return name;
-    }
-
-    static int PromptUserNumber()
-    {
-        Console.Write("Please enter your favorite number: ");
-        int number = int.Parse(Console.ReadLine());
-
-        return number;
-    }
-
-    static int SquareNumber(int number)
-    {
-        int square = number * number;
-        return square;
-    }
-
-    static void DisplayResult(string name, int square)
-    {
-        Console.WriteLine($"{name}, the square of your number is {square}");
+        myResume.Display();
     }
 }
